928e23c1107c7ea13c76ecc48d747062b51df994
MonsterDruide1 <5958456@gmail.com>
2020-12-12 16:07:08 +0100

    Lade Stundenplan aus Speicher

--- AMGApp/StundenplanViewController.swift
+++ AMGApp/StundenplanViewController.swift
@@ -24,9 +24,8 @@ class StundenplanViewController: UIViewController {
         if(weekday == 0){
             weekday = 7
         } //1=monday, not sunday
-        weekday-=1 //0-indexed
         createStundenplan(wochentag: weekday)
-        wochentagSelector.selectedSegmentIndex = weekday
+        wochentagSelector.selectedSegmentIndex = weekday-1
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
@@ -44,21 +43,24 @@ class StundenplanViewController: UIViewController {
     @IBAction func changeWochentag(_ sender: Any) {
         let wochentag = (sender as! UISegmentedControl).selectedSegmentIndex
         
-        createStundenplan(wochentag: wochentag)
+        createStundenplan(wochentag: wochentag+1)
     }
     
     func createStundenplan(wochentag: Int) {
         stackView.removeAllArrangedSubviews()
-        
-        let testView = makeStunde(stunde:1, fach: "Informatik", lehrer: "Ni", raum: "A104")
-        let testView1 = makeStunde(stunde:2, fach: "Informatik", lehrer: "Ni", raum: "A104")
-        
-        stackView.addArrangedSubview(testView)
-        stackView.addArrangedSubview(testView1)
+        let jsonString = UserDefaults.standard.string(forKey: "stundenplan"+wochentagToString(wochentag: wochentag))
+        if(jsonString == nil){
+            return
+        }
+        let stundenStrings: [String] = try! JSONDecoder().decode([String].self, from: (jsonString!.data(using: .utf8)!))
+        let stundenModels = stundenStrings.map{StundenplanEintragModel(allString: $0)}
+        stundenModels.map{
+            stackView.addArrangedSubview(makeStunde(stunde: $0.stunde, fach: $0.fachName, lehrer: $0.lehrer, raum: $0.raum, moveNeunteStunde: stundenModels.count >= 10))
+        }
         stackView.addHorizontalSeparators(color:.lightGray)
     }
     
-    func makeStunde(stunde: Int, fach: String, lehrer: String, raum: String) -> UIView {
+    func makeStunde(stunde: Int, fach: String, lehrer: String, raum: String, moveNeunteStunde: Bool) -> UIView {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         
@@ -83,7 +85,7 @@ class StundenplanViewController: UIViewController {
         view.addConstraint(NSLayoutConstraint(item: stundeLabel, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 5))
         
         let stundenZeitLabel = UILabel()
-        stundenZeitLabel.attributedText = NSAttributedString(string:stundeToTime(stunde:stunde), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 10)])
+        stundenZeitLabel.attributedText = NSAttributedString(string:stundeToTime(stunde:stunde, moveNeunteStunde: moveNeunteStunde), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 10)])
         stundenZeitLabel.textAlignment = .center
         stundenZeitLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(stundenZeitLabel)
@@ -125,7 +127,7 @@ class StundenplanViewController: UIViewController {
         return view
     }
     
-    func stundeToTime(stunde:Int) -> String {
+    func stundeToTime(stunde:Int, moveNeunteStunde:Bool) -> String {
         switch(stunde){
         case 1:
             return "7:45-8:30"
@@ -135,9 +137,58 @@ class StundenplanViewController: UIViewController {
             return "9:35-10:20"
         case 4:
             return "10:20-11:05"
-        #warning("Implement Zeiten der Stunden 5-10")
+        case 5:
+            return "11:25-12:10"
+        case 6:
+            return "12:15-13:00"
+        case 7:
+            return "13:15-14:00"
+        case 8:
+            return "14:00-14:45"
+        case 9:
+            if(moveNeunteStunde){
+                return "15:00-15:45"
+            } else {
+                return "14:45-15:30"
+            }
+        case 10:
+            return "15:45-16:30"
         default:
             return "ERROR"
+        }
+    }
+    
+    func wochentagToString(wochentag: Int) -> String{
+        switch(wochentag){
+        case 1:
+            return "Montag"
+        case 2:
+            return "Dienstag"
+        case 3:
+            return "Mittwoch"
+        case 4:
+            return "Donnerstag"
+        case 5:
+            return "Freitag"
+        default:
+            return "ERROR"
+        }
+    }
+    
+    class StundenplanEintragModel {
+        let stunde: Int
+        let fach: String
+        let lehrer: String
+        let raum: String
+        let fachName: String
+        
+        init(allString: String) {
+            let all = allString.components(separatedBy: "||")
+            stunde = Int(all[0]) ?? 1
+            fach = all[1]
+            lehrer = all[2]
+            raum = all[3]
+            fachName = all[4]
         }
     }
     
