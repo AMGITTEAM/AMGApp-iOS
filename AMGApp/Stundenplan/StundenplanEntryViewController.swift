47c75c49400445dd71263265055122101760e823
MonsterDruide1 <5958456@gmail.com>
2021-01-27 15:49:34 +0100

    Stundenplan: Vorschlag für vorhandene Stunden

--- AMGApp/Stundenplan/StundenplanEntryViewController.swift
+++ AMGApp/Stundenplan/StundenplanEntryViewController.swift
@@ -9,7 +9,9 @@
 import Foundation
 import UIKit
 
-class StundenplanEntryViewController: UIViewController, UITextFieldDelegate {
+class StundenplanEntryViewController: UIViewController, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var presetPicker: UIPickerView!
     @IBOutlet weak var fachname: UITextField!
     @IBOutlet weak var fachAbk: UITextField!
     @IBOutlet weak var lehrer: UITextField!
@@ -20,6 +22,7 @@ class StundenplanEntryViewController: UIViewController, UITextFieldDelegate {
     public var stunde: StundenplanViewController.StundenplanEintragModel? = nil
     public var delegate: StundenplanDay? = nil
     private var newHeightConstraint: NSLayoutConstraint? = nil
+    private var stundenPresets = [[String]]()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -37,6 +40,41 @@ class StundenplanEntryViewController: UIViewController, UITextFieldDelegate {
         self.hideKeyboardWhenTappedAround()
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        for i in 0...4 {
+            let jsonString = UserDefaults.standard.string(forKey: "stundenplan"+StundenplanDay.wochentagToString(wochentag: i)) ?? ""
+            do {
+                let stundenStrings = try JSONDecoder().decode([String].self, from: (jsonString.data(using: .utf8)!))
+                var stunden = stundenStrings.map{StundenplanViewController.StundenplanEintragModel(allString: $0)}
+                stunden.sort(by: {return $0.stunde < $1.stunde})
+                stunden.forEach{
+                    if(!stundenPresets.map{$0[0]}.contains($0.fachName) && $0.fachName.count>1){
+                        stundenPresets.append([$0.fachName, $0.fach, $0.lehrer, $0.raum])
+                    }
+                }
+            } catch {}
+            stundenPresets.sort{return $0[0]<$1[0]}
+        }
+        
+        presetPicker.dataSource = self
+        presetPicker.delegate = self
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stundenPresets.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return stundenPresets[row][0]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        fachname.text = stundenPresets[row][0]
+        fachAbk.text = stundenPresets[row][1]
+        lehrer.text = stundenPresets[row][2]
+        raum.text = stundenPresets[row][3]
     }
 
     @objc func keyboardWillShow(notification: NSNotification) {
